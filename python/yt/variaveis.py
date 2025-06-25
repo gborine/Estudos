d4669cf078106746970015958b62cb410c6399c8
g.borine <webmasterbrsuporte@gmail.com>
2025-06-25 11:58:30 -0300

    .

---
+++ python/yt/variaveis.py
@@ -0,0 +1,28 @@
+#Variaveis Tipo String
+nome = "Gustavo"
+print(nome)
+
+nome_composto = "Gustavo Borine"
+print(nome_composto)
+
+#Variaveis Tipo Numericas
+media = 0
+n1 = n2 = n3 = 5.5 #float
+print(n2)
+print(n1)
+n1 = 10
+print(n1)
+
+#Variaveis na msm linha
+nome , idade = "Claudio", 55
+print(nome,idade)
+
+# Função type
+type(n1)
+
+#Comentario em linha
+
+'''
+Comentario em bloco
+'''
+
